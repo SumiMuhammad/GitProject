4bf79f2b0843d14a5159f2529c49354beec0fa78
SumiMuhammad <muhamadtariq.mgm@gmail.com>
2018-07-28 13:02:50 +0700

    First commit to local Staging Area.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void myMethod1(String name) {
+
+        System.out.println("Your name is : "+ name);
+
+    }
+}
