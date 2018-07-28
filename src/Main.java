c7b139913e469256a62a1aa73c9f9255b4145053
SumiMuhammad <muhamadtariq.mgm@gmail.com>
2018-07-28 16:35:52 +0700

    Committing after resolving conflicts. Merge branch 'master' into LocalBranch_2
    # Conflicts:
    #	src/Main.java
    
    I am not sure that GIT is going Master with LocalBranch_2. Let see

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! The creator is the one who created this world.");
+        System.out.println("Hello World!. You are welcome to this new world.");
+        System.out.println("Hello World! The creator is the one who created this world.");
+        System.out.println("Hello World!. You are welcome to this new world.");
+        String name = "Sumaira Muhammad Tariq";
+
     }
 
     public void myMethod1(String name) {
 
-        System.out.println("Your name is : "+ name);
+        System.out.println("Your very good name is : "+ name +". I like your name.");
 
     }
 
     private StudentPojo createStudentObject(){
         StudentPojo stu = new StudentPojo();
+        System.out.println( "Student object created ..." );
         return stu;
     }
 }
