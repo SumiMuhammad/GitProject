3c5cb2765519d95696d73a94ae1de7b0f4431d32
SumiMuhammad <muhamadtariq.mgm@gmail.com>
2018-07-28 16:25:06 +0700

    Merge branch 'master' into LocalBranch_2
    # Conflicts:
    #	src/Main.java
    
    I am not sure that GIT is going Master with LocalBranch_2. Let see

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! The creator is the one who created this world.");
         System.out.println("Hello World!. You are welcome to this new world.");
+        System.out.println("Hello World! The creator is the one who created this world.");
+        System.out.println("Hello World!. You are welcome to this new world.");
+        String name = "Sumaira Muhammad Tariq";
+
+        new Main().myMethod1("Sumaira Muhammad Tariq");
     }
 
     public void myMethod1(String name) {
