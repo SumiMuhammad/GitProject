3ba0a273682128ce7f3cd1a48301b255981f6a81
SumiMuhammad <muhamadtariq.mgm@gmail.com>
2018-07-28 22:31:24 +0700

    Commit: Adding a new file (Employee.java) Commit on Master Branch

---
+++ src/Employee.java
@@ -0,0 +1,21 @@
+public class Employee {
+
+    private String employeeName;
+    private String employeeDept;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeDept() {
+        return employeeDept;
+    }
+
+    public void setEmployeeDept(String employeeDept) {
+        this.employeeDept = employeeDept;
+    }
+}
