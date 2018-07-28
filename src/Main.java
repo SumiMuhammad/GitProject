3629718be277aab9518482045b41356bd7ec14bb
SumiMuhammad <muhamadtariq.mgm@gmail.com>
2018-07-29 01:51:26 +0700

    Commit to Rebase Branch.

--- src/Main.java
+++ src/Main.java
@@ -38,5 +38,6 @@ public class Main {
 
         System.out.println("Gerrit has been integrated with Git repo");
         System.out.println(" Push was not going to Gerrit. Now I have added the SSH Key. Lets check if it works..");
+
     }
 }
